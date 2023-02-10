f74ed8614b411481bf9ce1e9631189f458e6dfed
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-10 18:40:31 +0200

    Create mixin into 'FrostWalkerEnchantment'

---
+++ src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/FrostWalkerEnchantmentMixin.java
@@ -0,0 +1,10 @@
+package top.offsetmonkey538.betterfrostwalker.mixin;
+
+import net.minecraft.enchantment.FrostWalkerEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(FrostWalkerEnchantment.class)
+public abstract class FrostWalkerEnchantmentMixin {
+
+
+}
