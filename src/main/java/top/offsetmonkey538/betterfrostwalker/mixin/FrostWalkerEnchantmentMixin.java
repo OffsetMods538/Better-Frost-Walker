ef90440eada513408475d5fc0ad6cc04c0a0207a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-10 18:44:25 +0200

    Disable the 'isOnGround' check

--- src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/FrostWalkerEnchantmentMixin.java
+++ src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/FrostWalkerEnchantmentMixin.java
@@ -1,10 +1,22 @@
 package top.offsetmonkey538.betterfrostwalker.mixin;
 
 import net.minecraft.enchantment.FrostWalkerEnchantment;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FrostWalkerEnchantment.class)
 public abstract class FrostWalkerEnchantmentMixin {
 
-
+    @Redirect(
+            method = "freezeWater",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/LivingEntity;isOnGround()Z"
+            )
+    )
+    private static boolean betterfrostwalker$makeFrostWalkerWorkWhileNotTouchingGround(LivingEntity instance) {
+        return true;
+    }
 }
