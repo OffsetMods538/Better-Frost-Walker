724a80c13815453e0a81e359a0ac599557bb5347
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-10 18:30:14 +0200

    Initial commit

---
+++ src/client/java/top/offsetmonkey538/betterfrostwalker/BetterFrostWalkerClient.java
@@ -0,0 +1,10 @@
+package top.offsetmonkey538.betterfrostwalker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterFrostWalkerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/top/offsetmonkey538/betterfrostwalker/BetterFrostWalker.java
@@ -0,0 +1,16 @@
+package top.offsetmonkey538.betterfrostwalker;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterFrostWalker implements ModInitializer {
+	public static final String MOD_ID = "better-frost-walker";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
