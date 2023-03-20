317a169db48e189781749280c9c0538b392b9083
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-20 16:37:11 +0200

    Better compatibility

--- src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/FrostWalkerEnchantmentMixin.java
+++ src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/FrostWalkerEnchantmentMixin.java
@@ -1,5 +1,7 @@
 package top.offsetmonkey538.betterfrostwalker.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -9,70 +11,52 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.Iterator;
 
 import static top.offsetmonkey538.betterfrostwalker.config.BetterFrostWalkerConfig.REQUIRED_LEVEL;
 
 @Mixin(FrostWalkerEnchantment.class)
 public abstract class FrostWalkerEnchantmentMixin {
 
-    @Unique
-    private static int betterfrostwalker$capturedLevel;
-
-    @ModifyVariable(
-            method = "freezeWater",
-            at = @At("HEAD"),
-            argsOnly = true,
-            index = 3
-    )
-    private static int betterfrostwalker$captureEnchantmentLevel(int level) {
-        betterfrostwalker$capturedLevel = level;
-        return level;
-    }
-
-    @Redirect(
+    @ModifyExpressionValue(
             method = "freezeWater",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/LivingEntity;isOnGround()Z"
             )
     )
-    private static boolean betterfrostwalker$makeFrostWalkerWorkWhileNotTouchingGround(LivingEntity instance) {
-        if (betterfrostwalker$capturedLevel != REQUIRED_LEVEL.get() && REQUIRED_LEVEL.get() != 0) return instance.isOnGround();
-        return true;
+    private static boolean betterfrostwalker$makeFrostWalkerWorkWhileNotTouchingGround(boolean original, @Local(argsOnly = true) int level) {
+        return (level == REQUIRED_LEVEL.get() || REQUIRED_LEVEL.get() == 0) || original;
     }
 
-    @ModifyConstant(
+    @ModifyArg(
             method = "freezeWater",
-            constant = @Constant(
-                    doubleValue = -1.0,
-                    ordinal = 0
+            index = 0,
+            at = @At(
+                    value ="INVOKE",
+                    target = "Lnet/minecraft/util/math/BlockPos;iterate(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;)Ljava/lang/Iterable;"
             )
     )
-    private static double betterfrostwalker$checkTwoBlocksBelowPlayer(double constant) {
-        if (betterfrostwalker$capturedLevel != REQUIRED_LEVEL.get() && REQUIRED_LEVEL.get() != 0) return constant;
-        return -2.0;
+    private static BlockPos betterfrostwalker$checkTwoBlocksBelowPlayer(BlockPos originalStartPos, @Local(argsOnly = true) int level) {
+        if (level != REQUIRED_LEVEL.get() && REQUIRED_LEVEL.get() != 0) return originalStartPos;
+        return originalStartPos.down();
     }
 
     @Inject(
             method = "freezeWater",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/block/BlockState;isAir()Z"
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
+                    target = "Lnet/minecraft/block/BlockState;getMaterial()Lnet/minecraft/block/Material;"
+            )
     )
-    private static void betterfrostwalker$replaceKelpAndSeagrassWithIce(LivingEntity entity, World world, BlockPos blockPos, int level, CallbackInfo ci, BlockState blockState, float f, BlockPos.Mutable mutable, Iterator var7, BlockPos blockPos2, BlockState blockState2) {
-        if (betterfrostwalker$capturedLevel != REQUIRED_LEVEL.get() && REQUIRED_LEVEL.get() != 0) return;
-        if (!blockState2.isAir()) return;
-        Block block = world.getBlockState(blockPos2).getBlock();
+    private static void betterfrostwalker$replaceKelpAndSeagrassWithIce(LivingEntity entity, World world, BlockPos blockPos, int level, CallbackInfo ci, @Local(ordinal = 1) BlockPos iteratedPos, @Local(ordinal = 0) BlockState iceState) {
+        if (level != REQUIRED_LEVEL.get() && REQUIRED_LEVEL.get() != 0) return;
+        Block block = world.getBlockState(iteratedPos).getBlock();
         if (block != Blocks.KELP && block != Blocks.SEAGRASS && block != Blocks.TALL_SEAGRASS) return;
-        world.setBlockState(blockPos2, blockState);
-        world.scheduleBlockTick(blockPos2, Blocks.FROSTED_ICE, MathHelper.nextInt(entity.getRandom(), 60, 120));
+        world.setBlockState(iteratedPos, iceState);
+        world.scheduleBlockTick(iteratedPos, Blocks.FROSTED_ICE, MathHelper.nextInt(entity.getRandom(), 60, 120));
     }
 }
