9e498316ea4e6699398a0f0a3d6429fa683618ff
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-16 12:49:56 +0300

    Fix crash when player tries to walk on powdered snow with boots that are enchanted, but not with frost walker.

--- src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/PowderSnowBlockMixin.java
+++ src/main/java/top/offsetmonkey538/betterfrostwalker/mixin/PowderSnowBlockMixin.java
@@ -3,15 +3,12 @@ package top.offsetmonkey538.betterfrostwalker.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.block.PowderSnowBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import static top.offsetmonkey538.betterfrostwalker.config.BetterFrostWalkerConfig.CAN_STAND_ON_POWDERED_SNOW;
+import static top.offsetmonkey538.betterfrostwalker.config.BetterFrostWalkerConfig.*;
 
 @Mixin(PowderSnowBlock.class)
 public abstract class PowderSnowBlockMixin {
@@ -21,9 +18,8 @@ public abstract class PowderSnowBlockMixin {
             at = @At("RETURN")
     )
     private static boolean betterfrostwalker$walkingOnPowderSnowWithFrostWalker(boolean original, Entity entity) {
-        ItemStack feetStack;
-        if (!CAN_STAND_ON_POWDERED_SNOW.get() || !(entity instanceof LivingEntity livingEntity && (feetStack = livingEntity.getEquippedStack(EquipmentSlot.FEET)).hasEnchantments())) return original;
+        if (!CAN_STAND_ON_POWDERED_SNOW.get() || !(entity instanceof LivingEntity livingEntity && EnchantmentHelper.hasFrostWalker(livingEntity))) return original;
 
-        return EnchantmentHelper.get(feetStack).get(Enchantments.FROST_WALKER) > 0 || original;
+        return true;
     }
 }
